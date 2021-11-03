8b3f7a506223f6eee33aa6c1c3321fb3424ae5c9
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2021-11-03 15:18:30 +0530

    Initial commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  // const ChatScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Chat'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, i) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This Works'),
+        ),
+      ),
+    );
+  }
+}
